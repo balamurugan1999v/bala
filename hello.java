a777be09435191676ab871ee9b89a4a19148a825
balamurugan1999v <31644214+balamurugan1999v@users.noreply.github.com>
2018-08-09 12:33:10 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class hello
+{
+public static void main(String arg[])
+{
+Scanner s=new Scanner(System.in);
+int n,i;
+n=s.nextInt();
+for(i=0;i<n;i++)
+{
+System.out.println("HELLO");
+}
+}
+}
