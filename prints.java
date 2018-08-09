d6dae926123a21aa48e8710a0993e14a9998d9c5
balamurugan1999v <31644214+balamurugan1999v@users.noreply.github.com>
2018-08-09 12:43:00 +0530

    Create prints.java

---
+++ prints.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class prints
+{
+public static void main(String arg[])
+{
+Scanner s=new Scanner(System.in);
+int n,c=0,i;
+n=s.nextInt();
+for(i=0;i<=n;i++)
+{
+c=c+i;
+}
+System.out.println(c);
+}
+}
