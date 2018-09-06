9818d493029161d7ee016e2d7f78005596c944a1
balamurugan1999v <31644214+balamurugan1999v@users.noreply.github.com>
2018-09-06 11:43:15 +0530

    Create mul.java

---
+++ mul.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class mul
+{
+public static void main(String[] arg)
+{
+Scanner s=new Scanner(System.in);
+int a,b,i;
+a=s.nextInt();
+for(i=1;i<=5;i++)
+{
+b=a*i;
+System.out.print(b+" ");
+}
+}
+}
