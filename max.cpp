eddab0b589501d6f48a5243986c793ad5663b20d
balamurugan1999v <31644214+balamurugan1999v@users.noreply.github.com>
2019-01-16 01:11:12 +0530

    Create max.cpp

---
+++ max.cpp
@@ -0,0 +1,18 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+int n,c;
+int a[n+1];
+cin>>n;
+for(int i=0;i<n;i++)
+{
+cin>>a[i];
+}
+for(int i=0;i<n-1;i++)
+if(a[i]>a[i+1])
+{
+c=a[i];
+}
+getch():
+}
